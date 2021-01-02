e2a4d5ed02e27c63808916151cc97509ec687215
@s❤Rainbow <Twilight_Princess_Sparkle@outlook.com>
2021-01-02 20:32:44 +0800

    Update ExampleMod.js

--- ExampleMod.js
+++ ExampleMod.js
@@ -5,4 +5,4 @@ updateURL = MCBBS-Loader:MCBBS-Loader-Batteries:ExampleMod:main
 description = 示例 Mod，适合新手上路测试或者开发基础
 version = 1.0.0
 */
-alert("欢迎使用 MCBBS Loader！\n示例模块已经成功安装了。")
+console.log("欢迎使用 MCBBS Loader！\n示例模块已经成功安装了。")
